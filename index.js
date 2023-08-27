7149dc906100540bcef9c1e147790d4c59d2c01c
SanjaySSrivathsa <118973113+SanjaySSrivathsa@users.noreply.github.com>
2023-08-27 19:45:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(  __dirname+"/index.html");
   });
